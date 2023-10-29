4bd1d0104b85db1fcce22020803b686b238734f6
Alain Basty <alain.basty@free.fr>
2023-10-29 20:57:01 +0100

    Added maze generator

--- test.gd
+++ test.gd
@@ -1,24 +1,32 @@
 extends Node2D
 
 var tree_scene = preload("res://tree.tscn")
+var block_scene = preload("res://ice_block.tscn")
+
+func add_scene_child(scene, c: int, l: int):
+	var instance = scene.instantiate()
+	instance.position = Vector2(c * 40, l * 40)
+	add_child(instance)
+
+func add_tree_child(c: int, l: int):
+	add_scene_child(tree_scene, c, l)
+
+func add_block_child(c: int, l: int):
+	add_scene_child(block_scene, c, l)
 
 func _ready():
-	var tree
 	for i in range(20):
-		tree = tree_scene.instantiate()
-		tree.position = Vector2(i * 40, 0)
-		add_child(tree)
-		tree = tree_scene.instantiate()
-		tree.position = Vector2(i * 40, 40 * 19)
-		add_child(tree)
+		add_tree_child(i, 0)
+		add_tree_child(i, 19)
 	for i in range(1, 19):
-		tree = tree_scene.instantiate()
-		tree.position = Vector2(0, i * 40)
-		add_child(tree)
-		tree = tree_scene.instantiate()
-		tree.position = Vector2(40 * 19, i * 40)
-		add_child(tree)
-
+		add_tree_child(0, i)
+		add_tree_child(19, i)
+	for l in range(1, 19, 2):
+		for c in range(1, 19, 2):
+			add_block_child(c, l)
+			match randi() % 5:
+				0,1: if c != 1: add_block_child(c, l + 1)
+				2,3: if l != 1: add_block_child(c + 1, l)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
