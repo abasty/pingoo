ca5ad597e9f824649dbf748843decd2cbe7572f5
Alain Basty <alain.basty@free.fr>
2023-10-31 16:23:35 +0100

    Removed RayCast2D nodes from santa.tscn and scripted raycasting

--- santa.gd
+++ santa.gd
@@ -2,10 +2,14 @@ extends Area2D
 
 @export var speed = 240
 @onready var target = position
+var ray
+
+func _ready():
+	ray = PhysicsRayQueryParameters2D.create(Vector2.ZERO, Vector2.ZERO, -1, [self])
+	ray.collide_with_areas = true
 
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	var collider: Object = null
+	var velocity = Vector2.ZERO
 
 	if target == position:
 		velocity.y = Input.get_axis("ui_up", "ui_down")
@@ -14,24 +18,26 @@ func _process(delta):
 			velocity.y = 0
 			if velocity.x < 0:
 				$AnimatedSprite2D.animation = "move-left"
-				collider = $LeftRay.get_collider()
 			else:
 				$AnimatedSprite2D.animation = "move-right"
-				collider = $RightRay.get_collider()
 			# end if
 		# end if
 		if velocity.y != 0:
 			if velocity.y < 0:
 				$AnimatedSprite2D.animation = "move-up"
-				collider = $UpRay.get_collider()
 			else:
 				$AnimatedSprite2D.animation = "move-down"
-				collider = $DownRay.get_collider()
 			# end if
 		# end if
-		if collider == null:
+
+		ray.from = global_position + Vector2(20, 20)
+		ray.to = ray.from + velocity * 40
+		var collider_dict = get_world_2d().direct_space_state.intersect_ray(ray)
+
+		if collider_dict.is_empty():
 			target = position + velocity * 40
 		else:
+			var collider = collider_dict.get("collider")
 			if collider.has_method("push") and Input.is_key_pressed(KEY_SPACE):
 				collider.push(velocity)
 			# end if
