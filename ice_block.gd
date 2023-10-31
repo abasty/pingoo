873870bfd884309656b2ba77d0ccaef3087d87cd
Alain Basty <alain.basty@free.fr>
2023-10-31 17:27:15 +0100

    Added Ice cube BREAKING state and related animation

--- ice_block.gd
+++ ice_block.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-enum State { IDLE, MOVING }
+enum State { IDLE, MOVING, BREAKING }
 
 @onready var speed = 600
 @onready var state = State.IDLE
@@ -12,6 +12,7 @@ func _ready():
 	ray = PhysicsRayQueryParameters2D.create(Vector2.ZERO, Vector2.ZERO, -1, [self])
 	ray.collide_with_areas = true
 	$AnimatedSprite2D.animation = "idle"
+# end func _ready
 
 func move(delta):
 	if target == position:
@@ -50,14 +51,31 @@ func move(delta):
 		$AnimatedSprite2D.frame = 0
 		$AnimatedSprite2D.stop()
 	# end if
+# end func move
 
 func _process(delta):
 	match state:
 		State.IDLE: return
 		State.MOVING: move(delta)
+	# end match
+# end func _process
 
 func push(v: Vector2):
 	if state == State.IDLE:
 		state = State.MOVING
 		velocity = v
+		ray.from = global_position + Vector2(20, 20)
+		ray.to = ray.from + velocity * 40
+		var collider = get_world_2d().direct_space_state.intersect_ray(ray)
+		if not collider.is_empty():
+			state = State.BREAKING
+			$AnimatedSprite2D.animation = "destroy"
+			$AnimatedSprite2D.play()
+
+		# end if
 	# end if
+# end func push
+
+
+func _on_animated_sprite_2d_animation_finished():
+	queue_free()
