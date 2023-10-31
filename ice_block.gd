5b5c348cb0e0323d345a2ec9e467f502360be357
Alain Basty <alain.basty@free.fr>
2023-10-31 19:49:46 +0100

    Code cleaning

--- ice_block.gd
+++ ice_block.gd
@@ -6,27 +6,21 @@ enum State { IDLE, MOVING, BREAKING }
 @onready var state = State.IDLE
 @onready var velocity = Vector2.ZERO
 @onready var target = position
-var ray
+@onready var sprite = $AnimatedSprite2D
+@onready var ray = PhysicsRayQueryParameters2D.create(Vector2.ZERO, Vector2.ZERO, -1, [self])
 
 func _ready():
-	ray = PhysicsRayQueryParameters2D.create(Vector2.ZERO, Vector2.ZERO, -1, [self])
 	ray.collide_with_areas = true
-	$AnimatedSprite2D.animation = "idle"
+	sprite.animation = "idle"
 # end func _ready
 
 func move(delta):
 	if target == position:
 		if velocity.x < 0:
-			$AnimatedSprite2D.animation = "left"
+			sprite.animation = "left"
 		elif velocity.x > 0:
-			$AnimatedSprite2D.animation = "right"
+			sprite.animation = "right"
 		# end if
-		if velocity.y < 0:
-			$AnimatedSprite2D.animation = "idle"
-		elif velocity.y > 0:
-			$AnimatedSprite2D.animation = "idle"
-		# end if
-
 		ray.from = global_position + Vector2(20, 20)
 		ray.to = ray.from + velocity * 40
 		var collider = get_world_2d().direct_space_state.intersect_ray(ray)
@@ -35,29 +29,28 @@ func move(delta):
 			target = position + velocity * 40
 		else:
 			state = State.IDLE
-			$AnimatedSprite2D.animation = "idle"
 		# end if
 	# end if
 
 	velocity = (target - position).normalized()
 
 	if velocity != Vector2.ZERO:
-		$AnimatedSprite2D.play()
+		if velocity.x != 0:
+			sprite.play()
+		# end if
 		position += (velocity * speed * delta).limit_length((target - position).length())
 		if position.is_equal_approx(target):
 			position = target
 		# end if
 	else:
-		$AnimatedSprite2D.frame = 0
-		$AnimatedSprite2D.stop()
+		sprite.pause()
 	# end if
 # end func move
 
 func _process(delta):
-	match state:
-		State.IDLE: return
-		State.MOVING: move(delta)
-	# end match
+	if state == State.MOVING:
+		move(delta)
+	# end if
 # end func _process
 
 func push(v: Vector2):
@@ -69,13 +62,12 @@ func push(v: Vector2):
 		var collider = get_world_2d().direct_space_state.intersect_ray(ray)
 		if not collider.is_empty():
 			state = State.BREAKING
-			$AnimatedSprite2D.animation = "destroy"
-			$AnimatedSprite2D.play()
-
+			sprite.animation = "destroy"
+			sprite.play()
 		# end if
 	# end if
 # end func push
 
-
 func _on_animated_sprite_2d_animation_finished():
 	queue_free()
+# end func
