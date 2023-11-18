2e250527e837c5102a98b5a10fb973f4d6599b2f
Alain BASTY <alain.basty@free.fr>
2023-11-18 16:00:54 +0100

    Changed score animation

--- score.gd
+++ score.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var digits = [ $p0, $p1, $p2, $p3, $p4, $p5 ]
 
-const speed = 100
+const speed = 600
 const DIGIT_HEIGHT = 26
 
 var target_value = 0
@@ -23,32 +23,49 @@ var value:
 func _ready():
 	value = 0
 	target_value = 123456
-# end func
+# end func _ready
 
 func _process(delta):
 	if value != target_value:
 		animate(delta)
 	# end if
-# end func
+# end func _process
 
 func animate(delta):
-	var target_value_copy = target_value
-	var changed = false
-	# for each digit, animate it to the target value
+	# Set rolling for all digits
+	var rolling = []
 	for i in range(digits.size()):
+		rolling.append(i == 0)
+	# end for
+
+	# Get digit values
+	var digit_values = []
+	for i in range(digits.size()):
+		digit_values.append(floor((digits[i].region_rect.position.y / DIGIT_HEIGHT)))
+	# end for
+
+	# for each digit, animate it to the next value
+	for i in range(digits.size()):
+		if !rolling[i]:
+			continue
+		# end if
 		var digit = digits[i]
-		var digit_value = floor(digit.region_rect.position.y / DIGIT_HEIGHT)
-		var target_digit_value = target_value_copy % 10
-		if digit_value != target_digit_value:
-			changed = true
-			digit.region_rect.position.y += speed * delta
-			if digit.region_rect.position.y >= 10 * DIGIT_HEIGHT:
-				digit.region_rect.position.y -= 10 * DIGIT_HEIGHT
+		digit.region_rect.position.y += speed * delta
+
+		# If digit has rolled over, reset it and roll the next digit
+		if digit.region_rect.position.y >= 10 * DIGIT_HEIGHT:
+			digit.region_rect.position.y -= 10 * DIGIT_HEIGHT
+			if i < digits.size() - 1: rolling[i + 1] = true
+		# end if
+
+		# If digit is not the first digit, compute rolling
+		if i != 0:
+			var digit_value = floor((digit.region_rect.position.y / DIGIT_HEIGHT))
+			# If digit value has changed, stop rolling
+			if digit_value != digit_values[i]:
+				rolling[i] = false
+				digit.region_rect.position.y = digit_value * DIGIT_HEIGHT
 			# end if
 		# end if
-		target_value_copy /= 10
 	# end for
-	if !changed:
-		value = target_value
-	# end if
-# end func
+# end func animate
