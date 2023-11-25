024e3ac6bfefcce508c2280f27e99f36217e9e9d
Alain BASTY <alain.basty@free.fr>
2023-11-25 16:06:40 +0100

    Simplified scoring

--- score.gd
+++ score.gd
@@ -1,9 +1,8 @@
 extends Node2D
 
 @onready var digits = [ $p0, $p1, $p2, $p3, $p4, $p5 ]
-@onready var rollings = [ false, false, false, false, false, false ]
 
-const speed = 800
+const speed = 300
 const DIGIT_HEIGHT = 26
 const DIGIT_DISPLAY_WIDTH = 12
 const DIGIT_DISPLAY_HEIGHT = 16
@@ -29,15 +28,6 @@ func set_display_value(new_value):
 	# end for
 # end func set_display_value
 
-# get the value of the display
-func get_display_value():
-	var v = 0
-	for i in range(digits.size()):
-		v += digits[i].region_rect.position.y / DIGIT_HEIGHT * pow(10, i)
-	# end for
-	return v
-# end func get_display_value
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if value != target_value:
@@ -46,34 +36,24 @@ func _process(delta):
 # end func _process
 
 func animate(delta):
-	rollings[0] = true
+	var tv = target_value
+	var changed = false
 
 	# for each digit, animate it to the next value
 	for i in range(digits.size()):
-		if !rollings[i]:
-			continue
-		# end if
-
 		var digit = digits[i]
 		var digit_value = floor(digit.region_rect.position.y / DIGIT_HEIGHT)
-
-		digit.region_rect.position.y += speed * delta
-
-		if digit.region_rect.position.y >= 10 * DIGIT_HEIGHT:
-			digit.region_rect.position.y -= 10 * DIGIT_HEIGHT
-			if i < digits.size() - 1: rollings[i + 1] = true
+		if tv % 10 != digit_value:
+			digit.region_rect.position.y += speed * delta
+			if digit.region_rect.position.y >= 10 * DIGIT_HEIGHT:
+				digit.region_rect.position.y -= 10 * DIGIT_HEIGHT
+			# end if
+			changed = true
 		# end if
-
-		# if the digit is not the first digit, and it has reached its target value, stop rolling
-		var new_digit_value = floor(digit.region_rect.position.y / DIGIT_HEIGHT)
-		if new_digit_value != digit_value:
-			rollings[i] = false
-			digit.region_rect.position.y = new_digit_value * DIGIT_HEIGHT
-		# end if
+		tv /= 10
 	# end for
 
-	if get_display_value() >= target_value:
+	if not changed:
 		set_display_value(target_value)
 	# end if
-
 # end func animate
