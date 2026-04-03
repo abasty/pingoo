a8e700e3d5a9f4772c6723d9c71e9c4cc30682dc
Alain BASTY <alain.basty@free.fr>
2026-04-03 20:40:01 +0200

    Add fullscreen toggle functionality and update menu layout

--- menu.gd
+++ menu.gd
@@ -1,9 +1,30 @@
 extends Control
 
+func _ready():
+	_update_fullscreen_button_text()
+# end func _ready
+
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://test.tscn")
 # end func _on_play_pressed
 
+func _on_fullscreen_pressed():
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	# end if
+	_update_fullscreen_button_text()
+# end func _on_fullscreen_pressed
+
 func _on_quit_pressed():
 	get_tree().quit()
 # end func _on_quit_pressed
+
+func _update_fullscreen_button_text():
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		$VBoxContainer/FullscreenButton.text = "Quitter le plein ecran"
+	else:
+		$VBoxContainer/FullscreenButton.text = "Plein ecran"
+	# end if
+# end func _update_fullscreen_button_text
